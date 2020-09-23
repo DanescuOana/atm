221e75b75db345534f26d9c0c98188eae6fb98a7
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-23 15:48:40 +0300

    create sqlmap for mybatis + support for mybatis

---
+++ src/main/java/com/tech/atm/support/MyBatisSupport.java
@@ -0,0 +1,14 @@
+package com.tech.atm.support;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.support.SqlSessionDaoSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class MyBatisSupport extends SqlSessionDaoSupport {
+
+    @Autowired
+    @Override
+    public void setSqlSessionFactory(SqlSessionFactory sqlSessionFactory) {
+        super.setSqlSessionFactory(sqlSessionFactory);
+    }
+}
