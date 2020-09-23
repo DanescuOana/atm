20fab68a749b562a45929e84e63e8156010e361b
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-23 12:43:54 +0300

    edit Currency java class

---
+++ src/main/java/com/tech/atm/model/Currency.java
@@ -0,0 +1,42 @@
+package com.tech.atm.model;
+
+import java.text.DecimalFormat;
+
+public class Currency {
+    private Long id;
+    private String name;
+    private String unit;
+    private DecimalFormat moneyDecimalFormat;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public DecimalFormat getMoneyDecimalFormat() {
+        return moneyDecimalFormat;
+    }
+
+    public void setMoneyDecimalFormat(DecimalFormat moneyDecimalFormat) {
+        this.moneyDecimalFormat = moneyDecimalFormat;
+    }
+}
