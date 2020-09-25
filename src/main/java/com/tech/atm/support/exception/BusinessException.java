c6e0a0236f3a64bb53f28a25000d13d2d306726e
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-25 10:48:14 +0300

    created BusinessException class

---
+++ src/main/java/com/tech/atm/support/exception/BusinessException.java
@@ -0,0 +1,48 @@
+package com.tech.atm.support.exception;
+
+import com.tech.atm.model.logic.BusinessMessage;
+
+import java.util.List;
+
+public class BusinessException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	private BusinessMessage businessMessage;
+
+	/**
+	 * Exception constructor.
+	 * @param msg exception message
+	 */
+	public BusinessException(String msg) {
+		super(msg);
+	}
+
+	/**
+	 * Nesting constructor.
+	 * @param cause the causing exception
+	 */
+	public BusinessException(Throwable cause) {
+		super(cause);
+	}
+
+	/**
+	 * Nesting constructor.
+	 * @param message - the error message
+	 * @param cause - the causing exception
+	 */
+	public BusinessException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public BusinessException(BusinessMessage businessMessage) {
+		this.businessMessage = businessMessage;
+	}
+
+	public BusinessMessage getBusinessMessage() {
+		return businessMessage;
+	}
+
+	public void setBusinessMessage(BusinessMessage businessMessage) {
+		this.businessMessage = businessMessage;
+	}
+}
