bc4ad2eb216b73c5d740e4f4fb49f8623691de5f
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-25 10:47:34 +0300

    create BusinessMessage class

---
+++ src/main/java/com/tech/atm/model/logic/BusinessMessage.java
@@ -0,0 +1,35 @@
+package com.tech.atm.model.logic;
+
+public class BusinessMessage {
+
+    private String message;
+    private Object[] params;
+
+    public BusinessMessage() {
+    }
+
+    public BusinessMessage(String message, Object[] params) {
+        this.message = message;
+        this.params = params;
+    }
+
+    public BusinessMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object[] getParams() {
+        return params;
+    }
+
+    public void setParams(Object[] params) {
+        this.params = params;
+    }
+}
