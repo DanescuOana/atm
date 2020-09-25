11d8d0ab5be00e0dc0ebfc9a86af338c400023a7
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-25 14:28:34 +0300

    added Swagger for rest endpoints http://localhost:8080/swagger-ui.html

---
+++ src/main/java/com/tech/atm/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.tech.atm.config;
+
+import io.swagger.annotations.Api;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(Api.class))
+               // .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("ATM RESTful API.")
+                .description("Complete ATM RESTful Webservices API documentation").version("0.0.0.1")
+                .contact(new Contact("Oana Danescu", null, null))
+                .build();
+    }
+}
