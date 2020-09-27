7dea344c201b316eb7ed0677a206d3344ae0fd71
Leanca Oana <oana.leanca@kepler-rominfo.com>
2020-09-27 13:43:25 +0300

    added junit methods for Card service

--- src/test/java/com/tech/atm/card/service/impl/CardServiceImplTest.java
+++ src/test/java/com/tech/atm/card/service/impl/CardServiceImplTest.java
@@ -1,7 +1,68 @@
 package com.tech.atm.card.service.impl;
 
-import org.springframework.boot.test.context.SpringBootTest;
+import com.tech.atm.card.service.CardService;
+import com.tech.atm.common.CommonServiceImplTest;
+import com.tech.atm.model.Card;
+import com.tech.atm.support.exception.BusinessRuntimeException;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@SpringBootTest
-public class CardServiceImplTest {
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardServiceImplTest extends CommonServiceImplTest {
+
+    @Autowired
+    CardService cardService;
+
+    @Test
+    public void checkExistCardByCardNumberOne()  {
+        Long cardNumber = 100006L;
+        Long cardNumberExistence = this.cardService.checkExistCardByCardNumber(cardNumber);
+
+        assertEquals(cardNumberExistence, 1L);
+    }
+
+    @Test
+    public void checkExistCardByCardNumberZero()  {
+        Long cardNumber = 100089L;
+
+        assertThrows(BusinessRuntimeException.class, () -> {
+            this.cardService.checkExistCardByCardNumber(cardNumber);
+        });
+    }
+
+
+    @Test
+    public void checkIfPinCorrectNull() {
+        Card card = new Card();
+        card.setCardNumber(cardNumbers.stream().count() > 0 ? cardNumbers.get(0) : null);
+        card.setPin(null);
+
+        assertThrows(BusinessRuntimeException.class, () -> {
+            this.cardService.checkIfPinCorrect(card);
+        });
+    }
+
+    @Test
+    public void checkIfPinCorrectTrue() {
+        Card card = new Card();
+        card.setCardNumber(cardNumbers.stream().count() > 0 ? cardNumbers.get(0) : null);
+        card.setPin(1223L);
+        Card cardFromDb = this.cardService.checkIfPinCorrect(card);
+
+        assertNotNull(cardFromDb);
+        assertEquals(cardFromDb, card);
+    }
+
+    @Test
+    public void checkIfPinCorrectFalse() {
+        Card card = new Card();
+        card.setCardNumber(cardNumbers.stream().count() > 0 ? cardNumbers.get(0) : null);
+        card.setPin(1235L);
+
+        assertThrows(BusinessRuntimeException.class, () -> {
+            this.cardService.checkIfPinCorrect(card);
+        });
+
+    }
 }
---
+++ src/test/java/com/tech/atm/common/CommonServiceImplTest.java
@@ -0,0 +1,80 @@
+package com.tech.atm.common;
+
+import com.tech.atm.model.Account;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Transactional
+@SpringBootTest
+public class CommonServiceImplTest {
+
+    protected Long userId;
+    protected String ibanCan;
+    protected String ibanCannot;
+    protected List<Account> accounts;
+    protected List<Long> cardNumbers;
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @BeforeEach
+    public void setUpTest() {
+        //insert into user
+        String username = "odanescu23";
+        String name = "Oana Danescu";
+
+        jdbcTemplate.update(
+                "INSERT INTO user (username, name) VALUES (?, ?)",
+                username, name
+        );
+
+        //insert into card
+        Long pin = 1223L;
+        Long cardNumberTrue = 100006L;
+        jdbcTemplate.update(
+                "INSERT INTO card (card_number, pin) VALUES (?, ?)",
+                cardNumberTrue, pin
+        );
+
+        Long cardNumberFalse = 100007L;
+        jdbcTemplate.update(
+                "INSERT INTO card (card_number, pin) VALUES (?, ?)",
+                cardNumberFalse, pin
+        );
+
+        //insert into account
+         ibanCan="RO49AAAA1B31007593840028";
+         ibanCannot="RO49AAAA1B31007593840029";
+        Double balance = 3445.45;
+        Long accountTypeIdCanWithdraw = 5L;
+        Long accountTypeIdCanNotWithdraw =4L;
+
+        userId = jdbcTemplate.queryForObject("select id from user where username ='"+username+"'", Long.class);
+
+        jdbcTemplate.update(
+                "INSERT INTO account (iban, balance_account, account_type_id, card_number, user_id) VALUES (?, ?, ?, ?, ?)",
+                ibanCan, balance, accountTypeIdCanWithdraw, cardNumberTrue, userId
+        );
+
+        jdbcTemplate.update(
+                "INSERT INTO account (iban, balance_account, account_type_id, card_number, user_id) VALUES (?, ?, ?, ?, ?)",
+                ibanCannot, balance, accountTypeIdCanNotWithdraw, cardNumberFalse, userId
+        );
+
+        cardNumbers = jdbcTemplate.queryForList("select card_number from account where user_id="+userId, Long.class);
+        accounts = this.getAccounts();
+    }
+
+    public List<Account> getAccounts() {
+        String sql = "select account.iban, account.balance_account balanceAccount, account.card_number cardNumber from account where user_id="+userId;
+
+        return jdbcTemplate.query(sql,
+                new BeanPropertyRowMapper<Account>(Account.class));
+    }
+}
